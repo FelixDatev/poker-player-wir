b54bfbff26bf80e8e12b723f18c69d5e882fdf5d
Felix Fischer <felix.fischer.datev@gmail.com>
2016-12-22 14:03:15 +0100

    urlencoding

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,14 +3,13 @@ package org.leanpoker.player;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import jdk.nashorn.internal.runtime.URIUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -82,7 +81,11 @@ public class Player {
 
             input = input.concat("]");
 
+            input = URLEncoder.encode(input, "UTF-8");
+
             System.out.println(input);
+
+
 
             URL url = new URL(input);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
