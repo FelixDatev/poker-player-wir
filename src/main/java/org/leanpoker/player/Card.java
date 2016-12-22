c47bdbf117c69467c93e6b1d60ad93b081c9d7a9
Felix Fischer <felix.fischer.datev@gmail.com>
2016-12-22 10:30:59 +0100

    Card erstellt.

---
+++ src/main/java/org/leanpoker/player/Card.java
@@ -0,0 +1,17 @@
+package org.leanpoker.player;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+/**
+ * Created by felixfischer on 22.12.16.
+ */
+public class Card {
+    public int rank;
+    public String suit;
+
+    public Card(JsonObject card) {
+        rank = card.get("rank").getAsInt();
+        suit = card.get("suit").getAsString();
+    }
+}
--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -11,13 +11,14 @@ public class Player {
     static final String VERSION = "1";
 
     public static int betRequest(JsonElement request) {
+        
         if (request.isJsonObject()) {
             JsonObject o = request.getAsJsonObject();
             JsonArray players = o.get("players").getAsJsonArray();
             for (JsonElement player: players ) {
                 JsonObject playerObj = player.getAsJsonObject();
                 if (playerObj.get("name").equals("WIR")) {
-
+                    playerObj.get("");
 
                     return 5;
                 }
