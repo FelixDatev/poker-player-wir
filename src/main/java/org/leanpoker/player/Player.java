b641cf1f75878edc83cde586521b1d0ed5d788a4
Felix Fischer <felix.fischer.datev@gmail.com>
2016-12-22 10:44:09 +0100

    keine if abfrage, errorprint von PlayerObj

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -11,19 +11,17 @@ public class Player {
     static final String VERSION = "1";
 
     public static int betRequest(JsonElement request) {
-        
-        if (request.isJsonObject()) {
             JsonObject o = request.getAsJsonObject();
             JsonArray players = o.get("players").getAsJsonArray();
             for (JsonElement player: players ) {
                 JsonObject playerObj = player.getAsJsonObject();
+                System.err.print(playerObj);
                 if (playerObj.get("name").equals("WIR")) {
                     playerObj.get("");
 
                     return 5;
                 }
             }
-        }
         return 2;
     }
 
