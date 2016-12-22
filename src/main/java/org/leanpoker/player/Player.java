b05d348e903558f7a7e5c763ffd2a49c9a77a9ae
Felix Fischer <felix.fischer.datev@gmail.com>
2016-12-22 10:09:05 +0100

    return 1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,15 +1,27 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "1";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        if (request.isJsonObject()) {
+            JsonObject o = request.getAsJsonObject();
+            JsonArray players = o.get("players").getAsJsonArray();
+            for (JsonElement player: players ) {
+                JsonObject playerObj = player.getAsJsonObject();
+                if (playerObj.get("name").equals("WIR")) {
+                    return 1;
+                }
+            }
+        }
+
     }
 
     public static void showdown(JsonElement game) {
